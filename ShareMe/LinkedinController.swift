a3528bbc919b176ff98bb955104c917578f0d77d
yann9602 <developer@Mac-de-Developer-2.local>
2017-01-03 19:24:42 +0100

    Linkedin
    commencement des motifs pour linkedin

---
+++ ShareMe/LinkedinController.swift
@@ -0,0 +1,18 @@
+//
+//  LinkedinController.swift
+//  ShareMe
+//
+//  Created by Developer on 03/01/2017.
+//  Copyright © 2017 Florian VIDAL. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class LinkedinConnectedController: UIViewController {
+  
+    @IBAction func setText(_ sender: UIButton) {
+        ReceiveText.text="lol"
+    }
+    @IBOutlet weak var ReceiveText: UILabel!
+    
+}
